26d2b1499a2a0059c8dac9d8c6a0fcc6f495c4b9
MaxAlberti <maxalberti@gmx.de>
2024-01-11 12:44:19 +0100

    Working prototype

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import re
 import os
 
 
+class ObsidianLink:
+    text: str
+    content: str
+
+    def __init__(self, text, content):
+        self.text = text
+        self.content = content
+
+
 def main():
     print("Please select your logseq folder with files to change.")
     logseq_dir = input("Input: ")
-    print(logseq_dir)
+    # print(logseq_dir)
     if logseq_dir.startswith('"'):
         logseq_dir = logseq_dir[1:]
     if logseq_dir.endswith('"'):
@@ -27,16 +36,26 @@ def main():
 
     for f in pages:
         if ".excalidraw." not in f.name:
+            print(f"Converting '{f.name}'...")
             file_content = ""
             with open(f.path, mode="r") as file:
                 file_content = file.read()
             image_links, exca_links, other_links = extract_links(
                 file_content, page_names
             )
-            print(image_links)
-            print(exca_links)
-            print(other_links)
-            input("--------")
+            new_content = file_content
+            for link in image_links:
+                new_content = new_content.replace(
+                    link.text, str(convert_image_link(link))
+                )
+            for link in exca_links:
+                new_content = new_content.replace(
+                    link.text, str(convert_excalidraw_link(link, page_names))
+                )
+            for link in other_links:
+                new_content = new_content.replace(link.text, link.text[1:])
+            with open(f.path, mode="w") as file:
+                file.write(new_content)
 
 
 def extract_links(text: str, page_names: list):
@@ -61,17 +80,33 @@ def extract_links(text: str, page_names: list):
             or link_content.endswith(".webp")
             or link_content.endswith(".svg")
         ):
-            image_links.add(link)
+            image_links.add(ObsidianLink(link, link_content))
         elif (
             link_content.endswith(".excalidraw")
             or link_content + ".excalidraw.md" in page_names
             or link_content + ".excalidraw" in page_names
         ):
-            exca_links.add(link)
+            exca_links.add(ObsidianLink(link, link_content))
         else:
-            other_links.add(link)
+            other_links.add(ObsidianLink(link, link_content))
 
     return list(image_links), list(exca_links), list(other_links)
+
+
+def convert_image_link(image_link: ObsidianLink) -> str:
+    link_name = image_link.content
+    return f"![{link_name}](../assets/{image_link.content})"
+
+
+def convert_excalidraw_link(exca_link: ObsidianLink, page_names: list) -> str:
+    link_name = exca_link.content
+    if link_name.endswith(".excalidraw"):
+        link_name = link_name[:-11]
+    elif link_name.endswith(".excalidraw.md"):
+        link_name = link_name[:-14]
+    elif link_name.endswith(".md"):
+        link_name = link_name[:-3]
+    return f"{{{{renderer excalidraw, {link_name}}}}}"
 
 
 if __name__ == "__main__":
